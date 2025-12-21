beb3193306770ca15dfd80461992629ffc219f21
JEricFarias <ericfarrias@hotmail.com>
2025-12-21 17:02:02 -0300

    feat: view target page
    A pagina ainda não esta completa. falta as ações.

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,11 +1,26 @@
-import { Button, Text, View } from "react-native";
-import { router } from "expo-router";
+import { View } from "react-native";
+
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { PageHeader } from "@/components/PageHeader";
+import { CurrencyInput } from "@/components/CurrencyInput";
 
 export default function Target() {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>Target</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira."
+      />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nome da meta"
+          placeholder="Ex. Viagem para praia, Samsung Watch"
+        />
+        <CurrencyInput label="Valor alvo (R$)" value={24587.57} />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
